50d87581e2be6789d7334b69be391c8670825f27
Prithwi <prithwi.online11@gmail.com>
2025-02-15 00:15:41 +0530

    emailjs

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -5,13 +5,8 @@ import {
   MapPin,
   Phone,
   Mail,
-  Facebook,
-  Twitter,
   Linkedin,
   FacebookIcon,
-  TwitchIcon,
-  XIcon,
-  TwitterIcon,
 } from "lucide-react";
 
 export default function ContactForm() {
@@ -27,7 +22,12 @@ export default function ContactForm() {
     setLoading(true);
 
     try {
-      await emailjs.send("service_id", "template_id", form, "service_id");
+      await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      );
       await fetch("http://localhost:5000/api/users/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
